ab1f3e62c366a3bd5fd0f948b609a29394d401f2
dhanu1713 <144720477+dhanu1713@users.noreply.github.com>
2023-09-15 17:22:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-def fact_rec(n):
-    if n==0 or n==1:
-      return 1
-    else:
-      return n*fact_rec(n-1)
-number=int(input("Enter a value:")) 
-res=fact_rec(number)
-print("The factorial of {} is {}.".format(number, res))
+class BankAccount:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return f"Deposited ${amount}. New balance: ${self.balance}"
+        else:
+            return "Invalid deposit amount."
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.balance}"
+        else:
+            return "Invalid withdrawal amount or insufficient funds."
+
+# Create an instance of BankAccount
+account = BankAccount()
+
+# Test deposit functionality
+print(account.deposit(100))  # Deposited $100. New balance: $100
+
+# Test withdrawal functionality
+print(account.withdraw(50))   # Withdrew $50. New balance: $50
+print(account.withdraw(75))   # Invalid withdrawal amount or insufficient funds
